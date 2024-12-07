011a281a32c6aa5b80abb68dc9d41a2184ba6062
Zeo <zmcsoltai@gmail.com>
2024-12-07 23:58:38 +0100

    Add bookbot report function

--- main.py
+++ main.py
@@ -2,7 +2,15 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
-    print(f"{num_words} words found in the document")
+    char_count = count_characters(text)
+
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document\n")
+    for x,y in char_count.items():
+        letter = x
+        count = y
+        print(f"The '{letter}' character was found {count} times")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -13,6 +21,17 @@ def get_book_text(path):
         return f.read()
 
 def count_characters(text):
-    return len(text)
+    lowered_string = text.lower()
+    character_count = {}
+
+    for i in lowered_string:
+        if i.isalpha():
+            if i in sorted(character_count):
+                character_count[i] += 1
+            else:
+                character_count[i] = 1  
+    sorted_count = dict(sorted(character_count.items()))    
+    return sorted_count
+
 
 main()
